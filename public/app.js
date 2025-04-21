8e99afa87bf6cc4e0b0e686f367e2fe93ef78230
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-21 08:43:28 -0600

    all post fateched and combined with new added post

--- public/app.js
+++ public/app.js
@@ -3,6 +3,28 @@ document.addEventListener("DOMContentLoaded", () => {
   const PostsBtn = document.getElementById("posts-btn");
   const outputDiv = document.getElementById("output");
 
+  //   handle load all post
+  PostsBtn.addEventListener("click", handleAddPosts);
+  async function handleAddPosts() {
+    try {
+      const res = await fetch("/posts");
+      if (res.ok) {
+        const posts = await res.json();
+        if (posts.length > 0) {
+          let postsHTML = "<h2>All Posts</h2><ul>";
+          posts.forEach((post) => {
+            postsHTML += `<li>Post Id:${post.id},  Title: ${post.title}</li>`;
+          });
+          postsHTML += "</ul>";
+          outputDiv.innerHTML = postsHTML;
+        }
+      }
+    } catch (err) {
+      console.log("Error: can't fetch any posts:", err);
+      outputDiv.textContent = "Failed to fetch posts.";
+    }
+  }
+
   // handle new post
   addPost.addEventListener("submit", handleSubmitPost);
 
@@ -12,14 +34,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const title = titleInput.value;
 
     try {
-      const response = await fetch("/posts", {
+      const res = await fetch("/posts", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ title }),
       })
-        .then((response) => response.json())
+        .then((res) => res.json())
         .then((newPost) => {
           outputDiv.innerHTML =
             `<p>New post added, Id: ${newPost.id}, Title: ${newPost.title}</p>` +
